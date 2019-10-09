a47024d6ae870f4b201de589158b113500a4fa97
Sahand Javid <sahand.javid@gmail.com>
2019-10-08 22:32:26 -0700

    export default

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import Parser from './Parser'
 import ArgCollection from './ArgCollection'
 
-export function argsCommandParser(
+export default function argsCommandParser(
   argv: string[] = process.argv.slice(2)
 ): ArgCollection {
   return Parser.parse(argv)
