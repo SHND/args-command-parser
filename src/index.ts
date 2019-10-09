950385ec3710ee0f6e1ef6e9c5fb8abbf0b08140
Sahand Javid <sahand.javid@gmail.com>
2019-10-08 22:23:02 -0700

    change default export on index.js to export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import Parser from './Parser'
 import ArgCollection from './ArgCollection'
 
-module.exports = function argsCommandParser(
+export function argsCommandParser(
   argv: string[] = process.argv.slice(2)
 ): ArgCollection {
   return Parser.parse(argv)
