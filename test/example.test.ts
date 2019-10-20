56b9e643d52bcb4ad60c5d1e26e404eca0e6c2e6
Sahand Javid <sahand.javid@gmail.com>
2019-10-19 18:15:05 -0700

    Add Istanbul

--- test/example.test.ts
+++ test/example.test.ts
@@ -1,7 +1,16 @@
 import { expect } from 'chai'
+import ShortSwitch from '../src/Args/ShortSwitch'
 
 describe('example tests', () => {
   it('true is true', () => {
-    expect(true).to.be.true
+    let x = ShortSwitch.isShortSwitch('-a')
+
+    expect(x).to.be.true
   })
 })
+
+// describe('example tests', () => {
+//   it('true is true', () => {
+//     expect(true).to.be.true
+//   })
+// })
