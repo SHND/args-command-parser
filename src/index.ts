48083b6f568dda933306705c195ddc05c2bbfa8f
Sahand Javid <sahand.javid@gmail.com>
2019-10-20 23:37:41 -0700

    export

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
 import Parser from './Parser'
+import { Data, RawData } from './Models'
 import ArgCollection from './ArgCollection'
+import Arg from './Args/Arg'
+import Command from './Args/Command'
+import Switch from './Args/Switch'
+import LongSwitch from './Args/LongSwitch'
+import ShortSwitch from './Args/ShortSwitch'
 
 export const parser = function argsCommandParser(
   argv: string[] = process.argv.slice(2)
 ): ArgCollection {
   return Parser.parse(argv)
+}
+
+export {
+  Data,
+  RawData,
+  ArgCollection,
+  Arg,
+  Command,
+  Switch,
+  LongSwitch,
+  ShortSwitch,
 }
